75cc547e04d56999257a3c7d455edad304dced57
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2026-03-19 14:48:25 +0100

    fix: tests need different time stamps now

--- src/core/bodies/moon.test.ts
+++ src/core/bodies/moon.test.ts
@@ -3,7 +3,7 @@ import { moonPhase } from "./moon";
 
 describe("moon phase", () => {
     it("is near full around 2000-01-21", () => {
-        const jd = 2451576.7; // approx full Moon
+        const jd = 2451565.2; // approx full Moon
         const phase = moonPhase(jd);
 
         // illuminatedFraction near 1
@@ -13,7 +13,7 @@ describe("moon phase", () => {
     });
 
     it("is near new around 2000-01-06", () => {
-        const jd = 2451561.5; // approx new Moon
+        const jd = 2451550.0; // approx new Moon
         const phase = moonPhase(jd);
         expect(phase.illuminatedFraction).toBeLessThan(0.1);
     });
